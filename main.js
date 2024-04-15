b606d82ba1a2f79aac023b72b09a32134ed48ed9
mcatalla <99306721+mcatalla@users.noreply.github.com>
2024-04-15 20:17:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ async function start() {
   const weatherData = await weatherPromise.json()
   const ourTemperature = weatherData.properties.periods[0].temperature;
 
-  document.querySelector('#temperature.output').textContent = ourTemperature;
+  document.querySelector('#temperature-output').textContent = ourTemperature;
 }
 
-start()
+start();
